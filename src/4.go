2cd3a29efa329bf97ae7f51507e2deecd4919084
vaantonio86 <vaantonio86@gmail.com>
2025-03-09 19:48:29 +0000

    Add 4.go

---
+++ src/4.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func solveMathProblem(input int) int {
+    // your solution here
+    return input * 2
+}
+
+func main() {
+    fmt.Println("Solution to math problem:", solveMathProblem(10))
+}
